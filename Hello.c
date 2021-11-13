9c56c0211e5cb190a3a6c7e16baf1f4135e1e598
youngbamer <0421mk@naver.com>
2021-11-13 21:59:11 +0900

    getchar 함수와 putchar 함수의 활용

--- Hello.c
+++ Hello.c
@@ -1,56 +1,33 @@
 #include <stdio.h>
 
-/*
-void SwapIntPtr(int* p1, int* p2) {
-	int* temp = p1;
-	p1 = p2;
-	p2 = temp;
+int checkChar(char ch) {
 
-	printf("*p1, *p2 : %d %d\n", *p1, *p2);
-	// 값이 아니라 주소값 Swap 하는 함수
-	// 복제된 p1, p2의 주소값이 바뀐 것
-}
+	if (ch >= 65 && ch <= 90) {
+		ch += 32;
+	}
+	else if (ch >= 97 && ch <= 122) {
+		ch -= 32;
+	}
+	else {
+		return -1;
+	}
 
-int main(void) {
-	
-	int num1 = 10, num2 = 20;
-	int* ptr1, * ptr2;
-	ptr1 = &num1, ptr2 = &num2;
-	printf("*ptr1, *ptr2 : %d %d\n", *ptr1, *ptr2);
-
-	SwapIntPtr(ptr1, ptr2);
-	
-	// ptr1의 주소 : 001FFCB8
-	// ptr2의 주소 : 00EFF9AC
-	printf("*ptr1, *ptr2 : %d %d\n", *ptr1, *ptr2);
-
-	return 0;
-}
-*/
-
-void SwapIntPtr(int** p1, int** p2) {
-	int* temp = *p1;
-	*p1 = *p2;
-	*p2 = temp;
-
-	printf("**p1, **p2 : %d %d\n", **p1, **p2);
-	// 값이 아니라 주소값 Swap 하는 함수
-	// 복제된 p1, p2의 주소값이 바뀐 것
+	return ch;
 }
 
 int main(void) {
 
-	int num1 = 10, num2 = 20;
-	int* ptr1, * ptr2;
-	ptr1 = &num1, ptr2 = &num2;
-	printf("*ptr1, *ptr2 : %d %d\n", *ptr1, *ptr2);
+	int ch;
+	ch = getchar();
+	ch = checkChar(ch);
 
-	SwapIntPtr(&ptr1, &ptr2);
-
-
-	// ptr1의 주소 : 001FFCB8
-	// ptr2의 주소 : 00EFF9AC
-	printf("*ptr1, *ptr2 : %d %d\n", *ptr1, *ptr2);
+	if (ch == -1) {
+		printf("잘못 입력하셨습니다.");
+		return -1;
+	}
+	
+	putchar(ch);
 
 	return 0;
+
 }
